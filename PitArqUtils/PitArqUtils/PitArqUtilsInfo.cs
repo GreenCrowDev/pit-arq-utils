ee0622acdfdc43e2ed2fcac285c7b858d5c43f4b
matricola787 <62719360+matricola787@users.noreply.github.com>
2022-07-23 15:58:04 +0200

    Create visual studio solution solution.

---
+++ PitArqUtils/PitArqUtils/PitArqUtilsInfo.cs
@@ -0,0 +1,57 @@
+﻿using Grasshopper.Kernel;
+using System;
+using System.Drawing;
+
+namespace PitArqUtils
+{
+    public class PitArqUtilsInfo : GH_AssemblyInfo
+    {
+        public override string Name
+        {
+            get
+            {
+                return "PitArq";
+            }
+        }
+        public override Bitmap Icon
+        {
+            get
+            {
+                //Return a 24x24 pixel bitmap to represent this GHA library.
+                return null;
+            }
+        }
+        public override string Description
+        {
+            get
+            {
+                //Return a short string describing the purpose of this GHA library.
+                return "";
+            }
+        }
+        public override Guid Id
+        {
+            get
+            {
+                return new Guid("8803b044-4cee-4d53-ba8d-991590b88e50");
+            }
+        }
+
+        public override string AuthorName
+        {
+            get
+            {
+                //Return a string identifying you or your company.
+                return "";
+            }
+        }
+        public override string AuthorContact
+        {
+            get
+            {
+                //Return a string representing your preferred contact details.
+                return "";
+            }
+        }
+    }
+}
